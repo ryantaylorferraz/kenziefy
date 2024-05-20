61ba0bab5a6bfcfe9567efdac9f6c89a1d82b34f
ryan <ryan.bilac2010@hotmail.com>
2024-05-20 10:38:58 -0300

    adicionando configuração cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import express from "express";
+import cors from "cors"
 import "express-async-errors";
 import "dotenv/config";
 import helmet from "helmet";
@@ -11,6 +12,8 @@ import { initSwagger } from "./configs/swagger";
 
 export const app = express();
 app.use(helmet());
+
+app.use(cors())
 
 export const initApp = () => {
   app.use(express.json());
